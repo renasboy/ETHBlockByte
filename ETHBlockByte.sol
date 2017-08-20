65c6704acee252def24a50b45f5cc1e505e6aedd
renasboy <renasboy@gmail.com>
2017-08-20 13:54:53 +0200

    Initial commit, working contract and prototype with index.html

---
+++ ETHBlockByte.sol
@@ -0,0 +1,79 @@
+pragma solidity ^0.4.14;
+
+contract ETHBlockByte {
+    bytes32 public name = 'ETHBlockByte';
+    address public owner;
+    uint256 public max_fee;
+    uint256 public create_block;
+    bytes1 public last_result;
+    bytes1 private block_pointer;
+    bytes1 private byte_pointer;
+
+    event Balance(uint256 _balance);
+    event Play(address indexed _sender, bytes1 _start, bytes1 _end, bytes1 _result, bool _winner, uint256 _time);
+    event Destroy();
+
+    function ETHBlockByte() {
+        owner = msg.sender;
+        create_block = block.number; 
+        block_pointer = 0xff;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier isPaid() {
+        require(msg.value > 0 && msg.value <= max_fee);
+        _;
+    }
+
+    function play(bytes1 _start, bytes1 _end) payable isPaid returns (bool) {
+        bool winner = false;
+        // cast start and end to uint8
+        uint8 start = uint8(_start);
+        uint8 end = uint8(_end);
+        // check range start to end
+        if (start == 0 || end < start) {
+            revert();
+        }
+        // get result block hash
+        bytes32 block_hash = block.blockhash(block.number - uint8(block_pointer));
+        // get result block byte
+        last_result = block_hash[uint8(byte_pointer) % 32];
+        // case result to uint8
+        uint8 result = uint8(last_result);
+        // set new pointers for new play
+        block_pointer = block_hash[31];
+        byte_pointer = block_hash[0];
+        // check for winner, ZERO is HOUSE
+        if (result > 0 && 
+            result >= start &&
+            result <= end) {
+            winner = true;
+            // there is a winner, calculate prize
+            uint256 range = end - start + 1;
+            uint256 percentage = 100 - (range * 100 / 255);
+            uint256 prize = msg.value * percentage / 100;
+            uint256 credit = msg.value + prize;
+            if (!msg.sender.send(credit)) {
+                return false;
+            }
+        }
+        max_fee = this.balance / 4;
+        Balance(this.balance);
+        Play(msg.sender, _start, _end, last_result, winner, now);
+        return true;
+    }
+
+    function destruct() isOwner {
+        Destroy();
+        selfdestruct(owner);
+    }
+
+    function () payable {
+        max_fee = this.balance / 4;
+        Balance(this.balance);
+    }
+}
